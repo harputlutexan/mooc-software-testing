b5fbcd814394ddebe9890726df9d6188f50cb847
omerusluogullari <omerfarukusluogullar@gmail.com>
2023-03-28 15:31:30 +0300

    New Test

--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,22 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MirrorTest {
+
+    @Test
+    public void testMirrorString(){
+        Mirror mirror = new Mirror();
+        String result = mirror.mirrorEnds("adadkada");
+        Assertions.assertEquals("ada",result);
+    }
+
+    @Test
+    public void testMirrorSymmetricString(){
+        Mirror mirror = new Mirror();
+        String result = mirror.mirrorEnds("adakada");
+        Assertions.assertEquals("ada",result);
+    }
+
 }
